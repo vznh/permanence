f0d2775452f4e9bfa535e42a06048bcf3e676163
Jason Son <jasonson2004@gmail.com>
2025-05-21 23:35:21 -0700

    config: edited for webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
